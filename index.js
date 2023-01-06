02f5c17a3ff0cb17c7bfc8061804af3af579142f
luis-moguea <luis.mogueadp@gmail.com>
2023-01-06 16:13:21 -0500

    general project updates

--- index.js
+++ index.js
@@ -2,4 +2,35 @@ import data from './data.json' assert { type: 'json' };
 
 const container = document.querySelector("#container")
 
-data.map()
+data.map(el => {
+    let element;
+    element = /*HTML*/`
+        <div class="main-js" id="item">
+            <div class="header">
+                <div class="title">
+                    <h2>dfbdbxfgfg</h2>
+                    <p class="total-follows">dfgndgdgdfg</p>
+                </div>
+                <div class="button">
+                    <p class="dark-word"></p>
+                </div>
+            </div>
+        </div>
+    `
+    container.innerHTML += element
+
+    el.item.map(info => {
+        if (info.type === "followers-stadistics"){
+            element = /*HTML*/`
+            <p class="dark-word">${info.followersCount}</p>
+            `
+        } else if (info.type === "general-stadistics"){
+            element = /*HTML*/`
+            <p class="dark-word">${info.followersCount}</p>
+            `
+        }
+
+        container.innerHTML += element
+    })
+    
+})
