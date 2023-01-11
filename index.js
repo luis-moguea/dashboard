bad677cb0994c26fbd3d3db934dd5c5126cd3304
luis-moguea <luis.mogueadp@gmail.com>
2023-01-11 18:41:24 -0500

    style updates

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ if(el.hero === "followers-hero") {
                 <div class="">
                     <div class="follows-info">
                         <div class="follows-info__header">
-                            <img class="follows-info__header--img" src="${info.networkIcon}" alt="">
+                            <img class="follows-info__header--img" src="${info.networkIcon}" alt="network-icon">
                             <p class="follows-info__header--text">${info.id}</p>
                         </div>
                         <div class="follows-info__content">
@@ -40,7 +40,7 @@ if(el.hero === "followers-hero") {
                             <p class="follows-info__content--text">${info.followersWord}</p>
                         </div>
                         <div class="follows-info__footer">
-                            <img class="follows-info__footer--img" src="${info.rowIcon}" alt="">
+                            <img class="follows-info__footer--img" src="${info.rowIcon}" alt="row">
                             <p class="follows-info__footer--text">${info.followersMetric + info.day}</p>
                         </div> 
                     </div>
@@ -63,21 +63,20 @@ if(el.hero === "followers-hero") {
 
     el.item.map(info => {
             element = /*HTML*/`
-            <div class="main-followers">
-                <div>
-                    <div>
+                <div class="main-general">
+                    <div class="main-general__metric">
                         <p>${info.metric}</p>
-                        <img src="${info.networkIcon}" alt="">
+                        <img src="${info.networkIcon}" alt="id">
                     </div>
-                    <div>
-                        <p>${info.metricCount}</p>
-                        <div>
-                            <img src="${info.rowIcon}" alt="">
-                            <p>${info.stadisticMetric}</p>
+                        <div class="main-general__content">
+                            <p>${info.metricCount}</p>
+                            <div class="main-general__content--cont">
+                                <img class="main-general__content--img" src="${info.rowIcon}" alt="row">
+                                <p>${info.stadisticMetric}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             `
              secondItem.innerHTML += element
         })}
