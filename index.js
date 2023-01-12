e1c784d70dcc1566045115b64363cf52635dfda8
luis-moguea <luis.mogueadp@gmail.com>
2023-01-12 18:42:17 -0500

    js updates

--- index.js
+++ index.js
@@ -9,81 +9,79 @@ const view = document.querySelector("#overview")
 data.map(el => {
     let element;
 
-if(el.hero === "followers-hero") {
-    element = /*HTML*/`
-        <div class="main-js" id="item">
-            <div class="header-js">
-                <div class="title">
-                    <h1 class="title--text">${el.title}</h1>
-                    <p class="total-follows">${el.followers}</p>
-                </div>
-                <div class="button">
-                    <p class="dark-word">${el.mode}</p>
-                </div>
-            </div>
-        </div>
-    `
-    header.innerHTML += element
-
-    el.item.map(info => {
-            element = /*HTML*/`
-        <div class="main__js">
-            <div class="main__js__network__color">
-                <div class="main__js__network__color" style="background: ${info.backColor};" >
-                    <div class="follows-info">
-                        <div class="follows-info__header">
-                            <img class="follows-info__header--img" src="${info.networkIcon}" alt="network-icon">
-                            <p class="follows-info__header--text">${info.id}</p>
-                        </div>
-                        <div class="follows-info__content">
-                            <h3 class="follows-info__content--count">${info.followersCount}</h3>
-                            <p class="follows-info__content--text">${info.followersWord}</p>
-                        </div>
-                        <div class="follows-info__footer">
-                            <img class="follows-info__footer--img" src="${info.rowIcon}" alt="row">
-                            <p class="follows-info__footer--text" style="color: ${info.color};">${info.followersMetric + info.day}</p>
-                        </div> 
+    if(el.hero === "followers-hero") {
+        element = /*HTML*/`
+            <div class="main-js" id="item">
+                <div class="header-js">
+                    <div class="title">
+                        <h1 class="title--text">${el.title}</h1>
+                        <p class="total-follows">${el.followers}</p>
+                    </div>
+                    <div class="button">
+                        <p class="dark-word">${el.mode}</p>
                     </div>
                 </div>
-            </div>
-        </div>
-            `
+            </div>`
+
+        header.innerHTML += element
+
+    el.item.map(info => {
+        element = /*HTML*/`
+            <div class="main__js">
+                <div class="main__js__network__color">
+                    <div class="main__js__network__color" style="background: ${info.backColor};" >
+                        <div class="follows-info">
+                            <div class="follows-info__header">
+                                <img class="follows-info__header--img" src="${info.networkIcon}" alt="network-icon">
+                                <p class="follows-info__header--text">${info.id}</p>
+                            </div>
+                            <div class="follows-info__content">
+                                <h3 class="follows-info__content--count">${info.followersCount}</h3>
+                                <p class="follows-info__content--text">${info.followersWord}</p>
+                            </div>
+                            <div class="follows-info__footer">
+                                <img class="follows-info__footer--img" src="${info.rowIcon}" alt="row">
+                                <p class="follows-info__footer--text" style="color: ${info.color};">${info.followersMetric + info.day}</p>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>`
     
-            firstItem.innerHTML += element
+        firstItem.innerHTML += element
 
     })} else if (el.hero === "general-hero"){
 
         element = /*HTML*/`
-        <div class="main-overview">
-            <h1 class="main-overview--text">
-                ${el.view + el.viewDay}
-            </h1>
-        </div>
-    `
-    view.innerHTML += element
+            <div class="main-overview">
+                <h1 class="main-overview--text">
+                    ${el.view + el.viewDay}
+                </h1>
+            </div>`
+
+        view.innerHTML += element
 
     el.item.map(info => {
-            element = /*HTML*/`
-                <div class="main-general">
-                    <div class="main-general__metric">
-                        <p>${info.metric}</p>
-                        <img src="${info.networkIcon}" alt="id">
-                    </div>
-                        <div class="main-general__content">
-                            <h2 class="main-general__content--count">${info.metricCount}</h2>
-                            <div class="main-general__content--cont">
-                                <img class="main-general__content--img" src="${info.rowIcon}" alt="row">
-                                <p class="main-general__content--text" style="color: ${info.color};">${info.stadisticMetric}</p>
-                            </div>
+        element = /*HTML*/`
+            <div class="main-general">
+                <div class="main-general__metric">
+                    <p>${info.metric}</p>
+                    <img src="${info.networkIcon}" alt="id">
+                </div>
+                    <div class="main-general__content">
+                       <h2 class="main-general__content--count">${info.metricCount}</h2>
+                        <div class="main-general__content--cont">
+                            <img class="main-general__content--img" src="${info.rowIcon}" alt="row">
+                            <p class="main-general__content--text" style="color: ${info.color};">${info.stadisticMetric}</p>
                         </div>
                     </div>
                 </div>
-            `
+            </div>`
 
-             secondItem.innerHTML += element
-        })}
+        secondItem.innerHTML += element
+        
+    })}
     
-
-    })
+})
     
 
