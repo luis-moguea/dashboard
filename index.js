05f65367ed3c915b73bdf10ecf4b7f333429e1f9
luis-moguea <luis.mogueadp@gmail.com>
2023-01-13 08:37:21 -0500

    js update

--- index.js
+++ index.js
@@ -1,4 +1,144 @@
-import data from './data.json' assert { type: 'json' };
+//import data from './data.json' assert { type: 'json' };
+
+
+const data = [
+    {
+        "hero": "followers-hero",
+        "title": "Social Media Dashboard",
+        "followers":"Total followers: 23,004",
+        "mode": "Dark Mode",
+        "item": [
+            {
+                "type": "followers-stadistics",
+                "backColor": "#198ff5",
+                "color": "#1db489",
+                "networkIcon": "images/icon-facebook.svg",
+                "id":"@nathanf",
+                "followersCount": "1987",
+                "followersWord": "followers",
+                "rowIcon": "images/icon-up.svg",
+                "followersMetric": "12 ",
+                "day": "Today"
+            },
+            {
+                "type": "followers-stadistics",
+                "backColor": "#1ca0f2",
+                "color": "#1db489",
+                "networkIcon": "images/icon-twitter.svg",
+                "id":"@nathanf",
+                "followersCount": "1044",
+                "followersWord": "followers",
+                "rowIcon": "images/icon-up.svg",
+                "followersMetric": "99 ",
+                "day": "Today"
+            },
+            {
+                "type": "followers-stadistics",
+                "backColor": "linear-gradient(90deg, #fdc468 0%, #df4996 100%)",
+                "color": "#1db489",
+                "networkIcon": "images/icon-instagram.svg",
+                "id":"@realnathanf",
+                "followersCount": "11k",
+                "followersWord": "followers",
+                "rowIcon": "images/icon-up.svg",
+                "followersMetric": "1099 ",
+                "day": "Today"
+            },
+            {
+                "type": "followers-stadistics",
+                "backColor": "#c4032a",
+                "color": "#dc414c",
+                "networkIcon": "images/icon-youtube.svg",
+                "id":"Nathan F.",
+                "followersCount": "8239",
+                "followersWord": "subscribers",
+                "rowIcon": "images/icon-down.svg",
+                "followersMetric": "144 ",
+                "day": "Today"
+            }
+        ]
+    },
+    {
+        "hero": "general-hero",
+        "view": "Overview - ",
+        "viewDay": "Today",
+        "item": [
+            {
+                "type": "general-stadistics",
+                "metric": "Page Views",
+                "networkIcon": "images/icon-facebook.svg",
+                "metricCount": "87",
+                "color": "#1db489",
+                "rowIcon": "images/icon-up.svg",
+                "stadisticMetric": "3%"
+            },
+            {
+                "type": "general-stadistics",
+                "metric": "Likes",
+                "networkIcon": "images/icon-facebook.svg",
+                "metricCount": "52",
+                "color": "#dc414c",
+                "rowIcon": "images/icon-down.svg",
+                "stadisticMetric": "2%"
+            },
+            {
+                "type": "general-stadistics",
+                "metric": "Likes",
+                "networkIcon": "images/icon-instagram.svg",
+                "metricCount": "5462",
+                "color": "#1db489",
+                "rowIcon": "images/icon-up.svg",
+                "stadisticMetric": "2257%"
+            },
+            {
+                "type": "general-stadistics",
+                "metric": "Profile Views",
+                "networkIcon": "images/icon-instagram.svg",
+                "metricCount": "52k",
+                "color": "#1db489",
+                "rowIcon": "images/icon-up.svg",
+                "stadisticMetric": "1375%"
+            },
+            {
+                "type": "general-stadistics",
+                "metric": "Retweets",
+                "networkIcon": "images/icon-twitter.svg",
+                "metricCount": "117",
+                "color": "#1db489",
+                "rowIcon": "images/icon-up.svg",
+                "stadisticMetric": "303%"
+            },
+            {
+                "type": "general-stadistics",
+                "metric": "Likes",
+                "networkIcon": "images/icon-twitter.svg",
+                "metricCount": "507",
+                "color": "#1db489",
+                "rowIcon": "images/icon-up.svg",
+                "stadisticMetric": "553%"
+            },
+            {
+                "type": "general-stadistics",
+                "metric": "Likes",
+                "networkIcon": "images/icon-youtube.svg",
+                "metricCount": "107",
+                "color": "#dc414c",
+                "rowIcon": "images/icon-down.svg",
+                "stadisticMetric": "19%"
+            },
+            {
+                "type": "general-stadistics",
+                "metric": "Total Views",
+                "networkIcon": "images/icon-youtube.svg",
+                "metricCount": "1407",
+                "color": "#dc414c",
+                "rowIcon": "images/icon-down.svg",
+                "stadisticMetric": "12%"
+            }
+        ]
+    }
+]
+
 
 const firstItem = document.querySelector("#first-item")
 const secondItem = document.querySelector("#second-item")
